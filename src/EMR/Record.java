ad0bbe5c1261ee0c5c7e15b14e21a0201e29e083
seryxme <coolbams24@yahoo.com>
2022-06-18 23:53:51 +0100

    EMR group project

--- src/EMR/Record.java
+++ src/EMR/Record.java
@@ -1,4 +1,12 @@
 package EMR;
 
+import java.util.ArrayList;
+
 public class Record {
+    private ArrayList<Test> medicalTests;
+
+    public Record() {
+        medicalTests = new ArrayList<>();
+
+    }
 }
--- src/EMR/User.java
+++ src/EMR/User.java
@@ -1,4 +1,27 @@
 package EMR;
 
+import java.util.ArrayList;
+
 public class User {
+    private ArrayList<Record> medicalRecords;
+
+    public User() {
+        medicalRecords = new ArrayList<>();
+    }
+
+    public ArrayList<Record> getMedicalRecords() {
+        return medicalRecords;
+    }
+
+    public void setMedicalRecords(Record record) {
+        medicalRecords.add(record);
+    }
+
+    public void deleteMedicalRecords(Record record) {
+        medicalRecords.remove(record);
+    }
+
+    public void deleteMedicalRecords(int index) {
+        medicalRecords.remove(index);
+    }
 }
